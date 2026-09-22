6f0528ff741a39255bcbe4ed6cff7e45e606aebc
vespanaa <vespana@gmx.fr>
2026-09-22 08:27:30 +0200

    Add files via upload

---
+++ EmotionDetection/__init__.py
@@ -0,0 +1,8 @@
+"""
+Paquete EmotionDetection para analisis de emociones con Watson NLP.
+"""
+
+from .emotion_detection import emotion_detector
+
+__all__ = ['emotion_detector']
+__version__ = '1.0.0'
