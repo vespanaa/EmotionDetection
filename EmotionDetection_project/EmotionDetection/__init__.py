07b3b936f2e3a5dab25c6efbd6db4ed2a69b7c2f
vespanaa <vespana@gmx.fr>
2026-09-22 08:26:02 +0200

    Delete EmotionDetection_project directory

--- EmotionDetection_project/EmotionDetection/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Paquete EmotionDetection para analisis de emociones con Watson NLP.
-"""
-
-from .emotion_detection import emotion_detector
-
-__all__ = ['emotion_detector']
-__version__ = '1.0.0'
